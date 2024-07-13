5c6d8feef862c9781835f0fe1eceaeb5fe4240c9
Sebastian Rojas <serogr99@gmail.com>
2024-07-13 10:57:22 -0600

    Entidad Tarea

--- src/main/java/com/metaphorce/api/tarea/model/Tarea.java
+++ src/main/java/com/metaphorce/api/tarea/model/Tarea.java
@@ -1,17 +1,57 @@
 package com.metaphorce.api.tarea.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "Tarea")
 public class Tarea {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
     private String title;
     private boolean completed;
 
+    public Tarea() {
+    }
+
+    public Tarea(Long id, String title, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
 }
